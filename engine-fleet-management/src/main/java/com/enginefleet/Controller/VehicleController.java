436d57f2023b6ee158883116f62ccc6940cb5295
yasmeenguled7815-bit <yasmeenguled7815@gmail.com>
2026-06-22 21:22:39 +0530

    updated vehicle controller class

--- engine-fleet-management/src/main/java/com/enginefleet/Controller/VehicleController.java
+++ engine-fleet-management/src/main/java/com/enginefleet/Controller/VehicleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +57,7 @@ public class VehicleController {
 		return vehicleRepository.save(vehicle);
 	}
 	
+	@DeleteMapping("/{id}")
 	public void deleteVehicle(@PathVariable Long id) {
 		vehicleRepository.deleteById(id);
 	}
